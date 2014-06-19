020fef033853e35a25ceb662215b3aae4cacdf53
Mohamedwertani <mohamedwertani@hotmail.fr>
2014-06-19 20:28:54 +0100

    Signed-off-by: Mohamedwertani <mohamedwertani@hotmail.fr>

---
+++ tn.edu.esprit.c1info2.codemasters.BestDeal/src/tn/edu/esprit/c1info2/codemasters/BestDeal/domain/User.java
@@ -0,0 +1,80 @@
+package tn.edu.esprit.c1info2.codemasters.BestDeal.domain;
+
+public class User {
+	private String id ;
+	private String firstName ;
+	private String lastName ;
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param firstName
+	 * @param lasttName
+	 */
+	public User(String firstName, String lasttName) {
+		this.firstName = firstName;
+		this.lastName = lasttName;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lasttName="
+				+ lastName + "]";
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof User))
+			return false;
+		User other = (User) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+	
+}
